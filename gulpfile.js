b5f5a2610c0c32a4c094a9a8b9bcce689dd92498
zbynioo85 <zbynioo85@gmail.com>
2019-12-07 00:12:52 +0100

    migrate to gulp 4, add gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,27 @@ var browserSync = require('browser-sync').create();
 var nunjacksRender = require('gulp-nunjucks-render');
 
 gulp.task('img', function () {
-  gulp.src('./src/img/**/*')
-    .pipe(gulp.dest('./web/img'))
+ return gulp.src('./src/img/**/*')
+    .pipe(gulp.dest('./web/img'));
+    
 });
 
 gulp.task('fonts', function () {
-    gulp.src('./src/fonts/**/*')
+  return  gulp.src('./src/fonts/**/*')
       .pipe(gulp.dest('./web/fonts'))
   });
 
 gulp.task('sass', function () {
-  gulp.src('./src/scss/style.scss')
+  return gulp.src('./src/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest('./web/css/'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 });
 
 
 gulp.task('js', function(){
-   gulp.src(['src/js/*'])
+  return gulp.src(['src/js/*'])
     .pipe(gulp.dest('./web/js/'));
 });
 
@@ -39,19 +40,20 @@ gulp.task('nunjucks', function() {
     .pipe(gulp.dest('./web'))
 });
 
-gulp.task('build', ['img', 'fonts', 'sass', 'nunjucks', 'js'], function() {
+gulp.task('build', gulp.series('img', 'fonts', 'sass', 'nunjucks', 'js', (done)=> {
     browserSync.init({
         server: "./web"
     });
-    gulp.watch("src/img/**/*", ['img']);
-    gulp.watch("src/fonts/**/*", ['fonts']);
-    gulp.watch("src/scss/**/*.scss", ['sass']);
-    gulp.watch("src/templates/**/*.html", ['nunjucks']);
-    gulp.watch("src/js/*.js", ['js']);
+    gulp.watch("src/img/**/*", gulp.series('img'));
+    gulp.watch("src/fonts/**/*", gulp.series('fonts'));
+    gulp.watch("src/scss/**/*.scss", gulp.series('sass'));
+    gulp.watch("src/templates/**/*.html", gulp.series('nunjucks'));
+    gulp.watch("src/js/*.js", gulp.series('js'));
     gulp.watch("web/*.html").on('change', browserSync.reload);
     gulp.watch("web/js/*.js").on('change', browserSync.reload);
     gulp.watch("web/img/**/*").on('change', browserSync.reload);
     gulp.watch("web/fonts/**/*").on('change', browserSync.reload);
-});
+    done();
+}));
 
-gulp.task('default', ['build']);
+gulp.task('default', gulp.series('build'));
